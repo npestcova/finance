796d66437392c338af1b7d931c246d8e6b5897ef
Natalia Pestcova <npestsova@clubautomation.com>
2018-03-01 10:37:21 -0600

    update table schema

--- finance/data/db.schema.sql
+++ finance/data/db.schema.sql
@@ -1,31 +1,35 @@
 CREATE TABLE `category` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar (255) not null default '',
-  `parent_id` int(11) null,
-  `exclude_from_cashflow` TINYINT(1) NOT NULL DEFAULT 0
-  INDEX `parent_id` (`parent_id`)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `parent_id` int(11) DEFAULT NULL,
+  `exclude_from_cashflow` tinyint(1) NOT NULL DEFAULT '0',
+  `type` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `account` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar (255) not null default ''
-);
-
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `transaction` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `date` DATE NULL,
-  `account_id` INT (11) null,
-  `category_id` INT (11) null,
-  `description` varchar(255) not null default '',
-  `amount` DECIMAL(8, 2) NOT NULL DEFAULT 0,
-  index `date` (`date`),
-  index `account_id` (`account_id`),
-  index `category_id` (`category_id`)
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `description` varchar(255) NOT NULL DEFAULT '',
+  `amount` decimal(8,2) NOT NULL DEFAULT '0.00',
+  PRIMARY KEY (`id`),
+  KEY `date` (`date`),
+  KEY `account_id` (`account_id`),
+  KEY `category_id` (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `keyword` (
-  `id` int(11) PRIMARY KEY AUTO_INCREMENT,
-  `keyword`  varchar(255) not null default '',
-  `category_id` INT (11) null
-);
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `keyword` varchar(255) NOT NULL DEFAULT '',
+  `category_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
